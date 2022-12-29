66add08ebece897b3b0b2419eb60d8ff5d8b937d
Forrest Loomis <forrest.loomis@docker.com>
2022-12-29 11:23:12 -0500

    add main command help text

--- cmd/root.go
+++ cmd/root.go
@@ -31,23 +31,21 @@ import (
 
 var cfgFile string
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "docker-init",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Use Docker with your existing projects",
+	Long: `Do you want to have all the great benefits of using Docker with an existing
+project and no idea where to start?
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+- Set up a Docker dev environment for this project
+- Bootstrap and configure your production-facing build artifact
+
+Docker init makes it simple!`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Doing the thing")
+	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
